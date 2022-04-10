a2025ab40411dd1a73745805dc550a0c1138eb34
Jules <j.marshallmiami@yahoo.com>
2022-04-10 15:11:30 -0500

    changes to pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+// display an empty page with search bar and header and clickable search button
+
+
+// once button is clicked we make an API call to OpenWeather API and receive weather data for specified city
+
+// Subpoint: city search is saved in localStorage for use on next page refresh
+
+// parse the JSON from the response and find the following: today's temp, today's uv, today's humidity and wind and store in variables for display
+
 // 
